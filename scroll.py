af5b699ce30ec73bdfd9a9f9235477f919aa0a8c
miyeong75 <70617943+miyeong75@users.noreply.github.com>
2020-12-01 13:03:44 +0900

    Add files via upload

---
+++ scroll.py
@@ -0,0 +1,17 @@
+from runtext import RunText
+import json
+
+def getdata(js_file):
+    with open (js_file,"r",encoding="utf-8") as f:
+        json_data = json.load(f)
+        text =''
+        for i in range(0,len(json_data)-1):
+            data_name = json_data[i]['지역이름']
+            data_num = json_data[i]['확진자수']
+            text = text + data_name + " : " + str(data_num) + "  "
+        return text
+
+
+run_text = RunText()
+run_text.my_text =getdata('koreaData_All_2020-11-26.js')
+run_text.process()
